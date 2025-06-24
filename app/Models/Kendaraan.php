8cc5598d78f4c697a526ed74a203571053fe79f3
erik <erikfirdaus03@gmail.com>
2025-06-24 10:00:30 +0700

    Add Kendaraan model and migration for vehicle data management

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Kendaraan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'nohp',
+        'plat_nomor',
+        'keluhan',
+        'tanggal',
+        'jam',
+    ];
 }
